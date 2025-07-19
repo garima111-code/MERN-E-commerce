70de74fbae4472da4132b9e8a52f697c1541e256
garima111-code <73467845+garima111-code@users.noreply.github.com>
2025-07-19 14:29:21 +0530

    Update Home.jsx

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -3,7 +3,6 @@ import Hero from '../components/Hero'
 import LatestCollection from '../components/LatestCollection'
 import BestSeller from '../components/BestSeller'
 import OurPolicy from '../components/OurPolicy'
-import NewsletterBox from '../components/NewsLetterBox'
 
 const home = () => {
   return (
